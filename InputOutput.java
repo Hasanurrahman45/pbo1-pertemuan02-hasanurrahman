b40cad6a87439201c3650964ffb6334a91e63066
hasanurrahman <rahman.hasanur45@gmail.com>
2021-03-31 09:42:42 +0700

    mengupdate file inputoutput kelas 4f

--- InputOutput.java
+++ InputOutput.java
@@ -2,11 +2,23 @@ import java.util.Scanner;
 
 public class InputOutput{
  public static void main(String[]args){
+  String nama;
+  int usia;
+  double ipk;
+
   Scanner scanner = new Scanner(System.in);
 
-  String nama;
-  System.out.print("Masukkan nama: ");
+  System.out.print("input nama : ");
   nama = scanner.nextLine();
-  System.out.print("Hai " + nama);
+
+  System.out.print("input usia : ");
+  usia = scanner.nextInt();
+
+  System.out.print("input ipk : ");
+  ipk = scanner.nextDouble();
+
+  System.out.println("Hai " + nama);
+  System.out.println("usia :" + usia);
+  System.out.println("ipk :" + ipk);
  }
 }
